beba553cc928f2a4a74bd1fa3eb017f3beadff25
CBDevelopments <71907197+CBDevelopments@users.noreply.github.com>
2020-09-26 19:51:21 +1000

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Navbar from "./components/Navbar/Navbar";
 import "./styles.css";
 
 function App() {
-  return <div className="App">Hi</div>;
+  return (
+    <div className="App">
+      <Navbar></Navbar>
+    </div>
+  );
 }
 export default App;
